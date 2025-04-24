0de83d940bb07aa2c62dc723db409d961a359747
shelbysanz <shelbsgt500@gmail.com>
2025-04-24 10:34:59 -0400

    feat: Package Hash Table function, init and class variables

--- PackageHt.py
+++ PackageHt.py
@@ -2,10 +2,17 @@ class PackageHt:
     """
     Package hash table that stores the package objects
     """
+    load_factor = 1.5
+    packages = 0
+
     def __init__(self, size=40, package_count=0):
         """
         O(1) - Inititalizes the hash table with 40 empty buckets
         """
+        self.size = size
+        self.table = [[] * range(self.size)]
+        self.package_count = package_count
+
     def get_package_bucket(self, package_id):
         """
         O(1) - Gets the hash of the package using the package id
