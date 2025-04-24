493bae02a2833d3dbe70eb8c188282f443395730
shelbysanz <shelbsgt500@gmail.com>
2025-04-24 08:08:13 -0400

    feat: Package.py class

---
+++ Package.py
@@ -0,0 +1,18 @@
+class Package:
+    """
+    Package class
+    """
+    def __str__(self):
+        """
+        O(1) - Return a string representation of the Package object
+        """
+    def assign_package(self, truck):
+        """
+        O(1) - Assigns truck to the package
+
+        returns: boolean, if package was successfully assigned
+        """
+    def deliver(self):
+        """
+        O(1) - Delivers the package
+        """
