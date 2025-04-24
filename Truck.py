317932e1a57a6b8c846c6fded9fa530b556a6aea
shelbysanz <shelbsgt500@gmail.com>
2025-04-24 08:32:49 -0400

    feat: Truck.py class

---
+++ Truck.py
@@ -0,0 +1,48 @@
+class Truck:
+
+    max_capacity = 16
+    hub_address = "4001 South 700 East"
+    avg_mph = 18
+    starting_mileage = 0
+    starting_location = 0
+    starting_distance = 0
+
+    def __init__(self, id, departure_time, driver):
+        """
+        O(1) - Initializing the truck object
+        - id: Integer, Truck identifier
+        - package_ids: List of Integers, package ids assigned to the truck
+        - total_mileage: Integer, total distance traveled by truck
+        - route: List of Strings, route taken by truck
+        """
+    def is_max_capacity(self):
+        """
+        O(1) - Checks if the truck is at max capacity
+
+        returns boolean if the truck is full or not
+        """
+    def update_mileage(self, distance_param):
+        """
+        O(1) - Updates the total mileage of the truck
+        """
+    def update_route(self, route, distance_param):
+        """
+        O(1) - Updates the route if the new route is an improvement
+        """
+    def assign_package(self, package_id):
+        """
+        O(1) - Assigns package to the truck
+        """
+    def on_time(self, distances, addresses):
+        """
+        O(n^2) - Tracks time for the truck and packages.
+        Will confirm if the packages will be delivered on time (by their deadline)
+
+        returns boolean if the package will be on time or not
+        """
+    def execute_route(self, query_time, distances):
+        """
+        O(n) - Getting the progress of the truck at the time of the query
+
+        returns the miles traveled and the location of the truck once the time of the query is reached
+        """
