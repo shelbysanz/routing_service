37f368d4983fdabb46b0022f4a9610bfb16971d7
shelbysanz <shelbsgt500@gmail.com>
2025-04-24 08:34:38 -0400

    feat: dispatch handler function

---
+++ helper.py
@@ -0,0 +1,16 @@
+def dispatch():
+    """
+    O(n^2) - Dispatch controller
+    - Loads trucks
+    - Implements 3-opt algorithm
+    - Finds routes and distances
+    - Ensures packages are on time
+    """
+
+    # import data using csv readers
+
+    # sorts packages
+
+    # loads trucks
+
+    # updates address that was wrong
