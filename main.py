935ed2eea4532341cdeb396df9adcb5b17bf4cd8
Shelby Sanchez <shelby@bluearrowmail.com>
2025-04-24 18:56:47 -0400

    feat: finalizing main

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from helper import dispatch, package_hashTable, initial_trucks
 from interface import interface_main
 
-dispatch(initial_trucks)
-interface_main(package_hashTable, initial_trucks)
+"""
+ This runs the program
+ It ensures it runs only when executed and not when simply imported
+
+ Created by: Shelby Sanchez-Herrera | Student ID: 012272973
+"""
+if __name__ == "__main__":
+    dispatch(initial_trucks)
+    interface_main(package_hashTable, initial_trucks)
